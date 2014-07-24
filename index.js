5da7fa3fe8c6531e1aac5e2f6c90ebde7f69ffba
Michael Robinson <mike@pagesofinterest.net>
2014-07-24 22:00:17 +1200

    Export Page model

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 module.exports = {
   initialise: require('./lib/initialise'),
   catchall: require('./lib/catchall'),
+  Page: function() {
+    return require('./lib/models/Page').model();
+  },
 };
