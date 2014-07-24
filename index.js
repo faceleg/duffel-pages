5a9bb3cc3e5388f80f8bb37faa4837d775fc0ef0
Michael Robinson <mike@pagesofinterest.net>
2014-07-25 09:01:01 +1200

    Admin tabs collector

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 module.exports = {
   initialise: require('./lib/initialise'),
   catchall: require('./lib/catchall'),
+  addAdminTab: require('./lib/admin-tabs').collector(),
   Page: function() {
     return require('./lib/models/Page').model();
   },
