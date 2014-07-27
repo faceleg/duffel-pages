40fa4288affd3652df4755e635911178d5bf02b2
Michael Robinson <mike@pagesofinterest.net>
2014-07-28 11:22:50 +1200

    Proper implementation of page controller

--- public/javascript/applications/admin/page-content/PageContentController.js
+++ public/javascript/applications/admin/page-content/PageContentController.js
@@ -3,17 +3,36 @@
 
   angular.module('admin-page-content.controllers')
   .controller('PageContentController', [
-    '$scope', 'Page',
-    function PageContentController($scope, Page) {
-      $scope.page = Page.get({ id: $scope.pageId });
-      $scope.cmsContent = {};
+    '$scope', 'Page', '$timeout', 'CMSPageContent',
+    function PageContentController($scope, Page, $timeout, CMSPageContent) {
 
+      $scope.blockingPromises = [];
+      $scope.page = null;
+      $scope.cmsPageContents = null;
+
+      $timeout(function() {
+        $scope.blockingPromises.push(Page.get({
+          id: $scope.page_id
+        }, function(page) {
+          $scope.page = page;
+
+          $scope.blockingPromises.push($scope.cmsPageContent = CMSPageContent.query({
+            owner_id: page.id,
+            owner_type: 'Page',
+            uri: page.uri
+          }, function(cmsPageContents) {
+            $scope.cmsPageContents = cmsPageContents;
+          }));
+        }));
+
+      });
 
       $scope.save = function() {
-        console.log('here');
-        console.log($scope.cmsContent);
+        $scope.cmsPageContents.forEach(function(cmsPageContents) {
+          $scope.blockingPromises.push(cmsPageContents.$save());
+        });
+        $scope.blockingPromises.push($scope.page.$save());
       };
-
     }
   ]);
 })();
